0c7ea26a391eb391ac85a085cbd68ae1feefafb4
sarathiringer <s.thiringer@gmail.com>
2022-12-01 08:04:30 +0100

    day 1

---
+++ 2022/day-1/day-1.py
@@ -0,0 +1,19 @@
+with open("input-1.txt", "r") as f:
+    lines = f.readlines()
+
+calories = []
+cal = 0
+for line in lines:
+    line = line.strip()
+    if line:
+        cal += int(line)
+    else: 
+        calories.append(cal)
+        cal = 0
+
+# Part 1
+print("The elf carrying the most is carrying", max(calories), "calories.")
+
+# Part 2
+calories.sort()
+print("The top three elves are carrying", sum(calories[-3:]), "calories worth of snacks. Good job elves!")
